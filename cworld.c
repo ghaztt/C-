0dc7dae1e84c8cd9632681f02fdadeff14cf8781
Alvezz <67349208+ghaztt@users.noreply.github.com>
2023-04-07 09:34:51 -0300

    Add files via upload

---
+++ cworld.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    // this is a comment 
+    /*
+    testing 
+    the
+    comments 
+    
+    */
+
+
+
+    printf(" a\t b\t c\n d\t e\t f\n g\t h\t i\t"); 
+     
+     return 0; 
+}     
