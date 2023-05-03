112af4d3ceb9962abe608ace51326eb74f9da063
Alvezz <67349208+ghaztt@users.noreply.github.com>
2023-05-02 21:22:28 -0300

    Add files via upload

---
+++ digite uma palavra.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+
+char texto[300];
+int n;
+
+printf("Escreva um texto:  ");
+scanf("%[^Aa]",texto);
+printf("o texto digitado foi %s\n",texto);
+return 0;
+}
+
+ 
+
+ 
+
+
+	
+	
+
+
+	
+	
+	
+	
+
