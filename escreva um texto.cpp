78f1df6753fb8a9298f1ee30e0147e0803d00512
Alvezz <67349208+ghaztt@users.noreply.github.com>
2023-05-02 21:27:58 -0300

    Add files via upload

---
+++ escreva um texto.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+
+char texto[100];
+int n;
+
+printf("Escreva um texto:  ");
+scanf("%[^\n]", texto);
+printf("o texto digitado foi %s\n",texto);
+return 0;
+}
+
+ 
+
+ 
+
+
+	
+	
+
+
+	
+	
+	
+	
+
