f4588a677a4748187c7a190f5b2a3803b80e0f46
Alvezz <67349208+ghaztt@users.noreply.github.com>
2023-05-02 21:22:44 -0300

    Add files via upload

---
+++ digite um número.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+
+char palavra[30];
+int n;
+
+printf("Digite uma palavra:  ");
+scanf("%s" ,palavra);
+printf("a palavra digitada foi %s\n" ,palavra);
+return 0;
+}
+
+ 
+
+ 
+
+
+	
+	
+
+
+	
+	
+	
+	
+
